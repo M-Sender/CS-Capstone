4353b8d0576d5cf3df23d02401daa8a6eceacc39
Max Sender <msender3489@gmail.com>
2022-04-12 14:47:23 -0500

    fixie

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import dash_bootstrap_components as dbc
 app = Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
-df = pd.read_csv('datasets/covid_US_full_by_day.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/M-Sender/CS-Capstone/master/covid_US_full_by_day.csv?token=GHSAT0AAAAAABTHEWZ4TNPTY5KECAWWNYRYYSV246Q')
 
 #def make_bar(x,y,)
 
