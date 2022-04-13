aa0333c9eefc4917481976220adfc2c22ac8213c
sbroth <sbroth@tulane.edu>
2022-04-13 11:18:50 -0500

    Improved readability.

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
+
+
 app = Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
-#df = pd.read_csv('https://raw.githubusercontent.com/M-Sender/CS-Capstone/master/covid_US_full_by_day.csv?token=GHSAT0AAAAAABTHEWZ4TNPTY5KECAWWNYRYYSV246Q')
 
-#def make_bar(x,y,)
 
 app.layout = html.Div(children=[
     html.H1(children='Music Trends During the Pandemic'),
