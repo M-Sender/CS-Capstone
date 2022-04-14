0893a62e255cf34c9f844c2521850e0a333e8085
Max Sender <msender3489@gmail.com>
2022-04-14 11:06:43 -0500

    clean

--- app.py
+++ app.py
@@ -41,37 +41,12 @@ app.layout = html.Div(children=[
         dbc.Row(html.H2(children='Sam Broth and Max Sender')),
         dbc.Row(html.H2(children='Mentor: Dr. Nick Mattei'))
     ]),className='text-center',style={'padding-top':'2%','color':'white'}),
-    dbc.Row(html.Div(children=[
-        dbc.Row(html.B(heading('1. Introduction'))), #make bold
-        dbc.Row(paragraph('Lorep Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.')),
-    ])),
-    dbc.Row(html.Div(children=[
-        dbc.Row(heading('2. Our Questions')),
-        dbc.Row(html.Ul(children=[
-            html.Li(paragraph('* What are the trends in music during the pandemic?')),
-            html.Li(paragraph('* What are the trends in music during the pandemic?')),
-            html.Li(paragraph('* What are the trends in music during the pandemic?')),
-        ]
-            
-            
-            )),
-        dbc.Row(paragraph('This leads us to our hypothesis:')),
-        dbc.Row(paragraph('lorep ipsum  simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a ga')),
-    ])),
-    dbc.Row(html.Div(children=[
-        dbc.Row(html.B(heading('3. Our Analysis'))),
-        
-        
-        
-        
-        
-    ])),
-    dcc.Graph(figure=px.line(df, x='date', y='vader', hover_data=['vader'],height=500,width=800)),
+    
     dbc.Row(html.Div(children=arr,)),
         
     
 ],style=css_dict['page'])
-                               
+                          
                         
 
 if __name__ ==  '__main__':
